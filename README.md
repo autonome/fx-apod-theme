69fe5b58f91c327c487ca5748066f86559f25de3
dietrich ayala <autonome@gmail.com>
2018-06-20 19:02:21 -0700

    bang

---
+++ README.md
@@ -0,0 +1,10 @@
+# APOD Theme for Firefox
+
+TODO:
+
+* add better accent and text color choice (detect and generate?)
+* add new tab page showing photo
+* add choice of https://www.nasa.gov/multimedia/imagegallery/iotd.html
+* add choice of https://www.earthobservatory.nasa.gov/IOTD/
+* (or cycle through all?)
+* use NASA API instead of scraping: https://api.nasa.gov/api.html
---
+++ background.js
@@ -0,0 +1,35 @@
+(async () => {
+  // Check daily for new image
+	browser.alarms.onAlarm.addListener(update);
+	browser.alarms.create('daily', { periodInMinutes: 24 * 60 });
+
+  // Fire immediately after install/startup
+  await update();
+})();
+
+async function update() {
+	let url = 'https://apod.nasa.gov/apod/astropix.html';
+  let resp = await fetch(url);
+  let text = await resp.text();
+  let matches = new RegExp('SRC="(.*)"').exec(text);
+  if (matches.length == 2) {
+	  let image = 'https://apod.nasa.gov/apod/' + matches[1];
+    console.log('GOT IT', image);
+    let resp = await fetch(image);
+    let blob = await resp.blob();
+    let uri = URL.createObjectURL(blob);
+    var reader  = new FileReader();
+    reader.addEventListener("load", function () {
+      browser.theme.update({
+        images: {
+          headerURL: reader.result
+        },
+        colors: {
+          textcolor: '#fff',
+          accentcolor: '#000'
+        }
+      });
+    }, false);
+    reader.readAsDataURL(blob);
+  }
+}
